a04b2b7937e31b4624c2c7a9b5bd356196ddc25d
fatmaboodai <Fatmaboodai@hotmail.com>
2023-11-15 23:46:27 +0300

    draft

--- app.py
+++ app.py
@@ -4,36 +4,74 @@ from PIL import Image
 from io import BytesIO
 from time import time
 from flask_socketio import SocketIO
+import numpy as np
 from ultralytics import YOLO
 from roboflow import Roboflow
 import cv2 as cv
 rf = Roboflow(api_key="7Hl9FLL5IgTbW6A70Nue")
 project = rf.workspace("mindcue").project("combo-dataset")
 model = project.version(3).model
+model = YOLO('yolov8n.onnx')
 
 app = Flask(__name__)
-# model = YOLO('yolov8n.onnx')
 socketio = SocketIO(app)
 latest_frame_timestamp = 0  # Timestamp to track the latest frame update
 latest_frame = None
 
-
 @app.route('/upload_frame', methods=['POST'])
 def upload_frame():
-    global latest_frame
     frame = request.files['frame']
-
+    frame.save('image.png')
+    print(frame)
     try:
-        with Image.open(frame.stream) as img:
-            # Process the frame with YOLO model
-            retval, buffer = cv.imencode('.jpg', img)
-            project.upload(buffer)
-
-            # detected_names = results.names
-        
-            return {'detected_objects': model.predict()}
+        # Convert PIL Image to OpenCV format
+        with Image.open("image.png") as img:
+            predictions = model.predict(img)
+            # Print predictions to the terminal
+            print(predictions.json())
+            return {'detected_objects': predictions.json()}
     except Exception as e:
         return {"error": str(e)}
 
 if __name__ == '__main__':
     socketio.run(app, port=8080)
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, request, render_template_string
+# import base64
+# from PIL import Image
+# from io import BytesIO
+# from time import time
+# from flask_socketio import SocketIO
+# from ultralytics import YOLO
+
+# app = Flask(__name__)
+# model = YOLO('yolov8n.onnx')
+# socketio = SocketIO(app)
+# latest_frame_timestamp = 0  # Timestamp to track the latest frame update
+# latest_frame = None
+
+# @app.route('/upload_frame', methods=['POST'])
+# def upload_frame():
+#     frame = request.files['frame']
+#     try:
+#         # Convert PIL Image to OpenCV format
+#         with Image.open(frame.stream) as img:
+#            results = model.predict(img)
+           
+#         return {'detected_objects':results.names}
+#     except Exception as e:
+
+#         return {"error": str(e)}
+
+# if __name__ == '__main__':
+#     socketio.run(app, port=8080)
--- model.py
+++ model.py
@@ -1,12 +1,15 @@
-from roboflow import Roboflow
+# from roboflow import Roboflow
 
-rf = Roboflow(api_key="7Hl9FLL5IgTbW6A70Nue")
-project = rf.workspace("mindcue").project("combo-dataset")
-model = project.version(3).model
+# rf = Roboflow(api_key="7Hl9FLL5IgTbW6A70Nue")
+# project = rf.workspace("mindcue").project("combo-dataset")
+# # model = project.version(3).model
+# from inference.models.utils import get_roboflow_model
 
+# img_url = "./test.jpeg"
 
-img_url = "./test.jpeg"
-
-predictions = model.predict(img_url)
-
-print(predictions)
+# # predictions = model.predict(img_url)
+# model = get_roboflow_model(
+#     api_key="rf_DM65bHNp6cbjPle0UTgOu1Aew442",
+#     model_id="combo-dataset/3"
+# )
+# print(model.infer(image =img_url))
