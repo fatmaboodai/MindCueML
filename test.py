2c8a442c92557975bc14147c5f9599a973f1bfe0
fatmaboodai <76260193+fatmaboodai@users.noreply.github.com>
2023-11-17 20:08:04 +0300

    websocket new connection

--- test.py
+++ test.py
@@ -1,4 +1,63 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from ultralytics import YOLO
+from PIL import Image
+import io
+import base64
+model = YOLO('yolov8n.pt')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('drafts.html')  # Make sure 'index.html' exists in your templates folder
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('send_frame')
+def handle_frame(data):
+    print("video recived")
+    print(type(data))
+
+    # Predict using YOLO
+    # results = model.predict(image)
+    # print(results)
+
+    # Process the frame as needed
+    emit('response_event', 'coming from flask i got your frame ')
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=9000
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # from flask import Flask
+
 # from flask_socketio import SocketIO
 # from roboflow import Roboflow
 # import asyncio
@@ -45,31 +104,31 @@
 # if __name__ == '__main__':
 #     socketio.run(app, host='127.0.0.1', port=5500)
 
-from flask import Flask
-from flask_socketio import SocketIO
-import base64
-import os
-import asyncio
-from ultralytics import YOLO
-model = YOLO('yolov8n.pt')
-app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+# from flask import Flask
+# from flask_socketio import SocketIO
+# import base64
+# import os
+# import asyncio
+# from ultralytics import YOLO
+# model = YOLO('yolov8n.pt')
+# app = Flask(__name__)
+# socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def index():
-    return "WebSocket Server"
+# @app.route('/')
+# def index():
+#     return "WebSocket Server"
 
-@socketio.on('upload_frame')
-def handle_upload_frame(data):
-    # Convert ArrayBuffer to bytes
-    image_data = base64.b64decode(data)
-    print(image_data)
-    print(model.predict(image_data))
-    # Process the image data (e.g., save to file, run ML model, etc.)
-    # You can use a similar approach as before for processing
+# @socketio.on('upload_frame')
+# def handle_upload_frame(data):
+#     # Convert ArrayBuffer to bytes
+#     image_data = base64.b64decode(data)
+#     print(image_data)
+#     print(model.predict(image_data))
+#     # Process the image data (e.g., save to file, run ML model, etc.)
+#     # You can use a similar approach as before for processing
 
-    # Send response back to client if needed
-    socketio.emit('response', {'message': 'Frame received'})
+#     # Send response back to client if needed
+#     socketio.emit('response', {'message': 'Frame received'})
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+#     socketio.run(app, host='0.0.0.0', port=8080)
